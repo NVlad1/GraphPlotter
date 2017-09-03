af105d6bb9910be77839cab144a413e12af74a30
NVlad1 <NVlad1@ukr.net>
2017-09-03 12:19:17 +0300

    clear repository

--- MathApp/app/src/main/java/com/nvlad/mathapp/AnalysisException.java
+++
@@ -1,7 +0,0 @@
-package com.nvlad.mathapp;
-
-/**
- * Created by Vlad on 08.02.2016.
- */
-public class AnalysisException extends Exception{
-}
--- MathApp/app/src/main/java/com/nvlad/mathapp/UnknownException.java
+++
@@ -1,7 +0,0 @@
-package com.nvlad.mathapp;
-
-/**
- * Created by Vlad on 28.12.2015.
- */
-public class UnknownException extends Exception{
-}
