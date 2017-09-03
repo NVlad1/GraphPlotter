1f175dd1496f3314ef3ad9e78480b8def7ca56ba
NVlad1 <NVlad1@ukr.net>
2017-09-03 12:07:30 +0300

    Revert "Add files via upload"
    This reverts commit 21c4a73ed45d43d8a5b4f70f6f4f9002c604c528.

--- MathApp/app/src/main/java/com/nvlad/mathapp/Exception/AnalysisException.java
+++
@@ -1,7 +0,0 @@
-package com.nvlad.mathapp.Exception;
-
-/**
- * Created by Vlad on 08.02.2016.
- */
-public class AnalysisException extends Exception{
-}
--- MathApp/app/src/main/java/com/nvlad/mathapp/Exception/UnknownException.java
+++
@@ -1,7 +0,0 @@
-package com.nvlad.mathapp.Exception;
-
-/**
- * Created by Vlad on 28.12.2015.
- */
-public class UnknownException extends Exception{
-}
